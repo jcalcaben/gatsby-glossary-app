6895659d452e3d8c3531d2116f95c00342bd5994
James Calcaben <jcalcaben@gmail.com>
2019-01-07 04:53:22 +0000

    Fix layout width for desktop mode

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,6 @@
 import { GlossaryTerm, TermsList } from './GlossaryTerms'
 import Scrim from './Scrim'
 import Header from './Header'
-import TermsDataProvider from './TermsDataProvider'
 import Panel from './Panel'
 
-export { Header, GlossaryTerm, TermsList, Scrim, TermsDataProvider, Panel }
+export { Header, GlossaryTerm, TermsList, Scrim, Panel }
