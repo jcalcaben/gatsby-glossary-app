3f9705d0f279bdf2a9c5e3741c574399ded94780
James Calcaben <jcalcaben@gmail.com>
2019-01-08 05:07:43 +0000

    Separate glossary fetch logic into its own react hook class

--- src/layouts/GlossaryTerms/term.js
+++ src/layouts/GlossaryTerms/term.js
@@ -1,30 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import GlossaryTerm from './glossaryTerm'
 import PropTypes from 'prop-types'
 import styles from './glossaryTerm.module.css'
-
-const fetchJsonData = (publicURL, callback) => {
-  fetch(publicURL)
-    .then(response => {
-      return response.json()
-    })
-    .then(callback)
-    .catch(error => {
-      console.log('Error fetching: ' + publicURL)
-      console.log(error)
-    })
-}
+import useTermData from './useTermData'
 
 const Term = ({ anchorId, publicURL }) => {
-  const [data, setData] = useState()
-
-  useEffect(() => {
-    if (data == null) {
-      fetchJsonData(publicURL, data => {
-        setData(data)
-      })
-    }
-  })
+  const data = useTermData(publicURL)
 
   if (data == null) {
     return (
