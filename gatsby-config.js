feca657c51195bf29e66092533f0368c4fd7645c
Tommy Wiebell <twiebell@adobe.com>
2019-04-10 16:39:45 -0500

    - Add Markdown transformer - Convert page generation from node to markdown query - Stub out rough term component

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-transformer-json',
-
+    'gatsby-transformer-remark',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
